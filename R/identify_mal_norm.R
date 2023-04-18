47f11dfbb3c9de0838fcb1e7f34b3740552bb440
Kristen Michelle Nader <61286548+kris-nader@users.noreply.github.com>
2023-04-18 14:49:48 +0300

    Create identify_mal_norm.R

---
+++ R/identify_mal_norm.R
@@ -0,0 +1,11 @@
+######################################################################################
+## Get functions for the identification of malignant and healthy clusters ##
+######################################################################################
+# 
+# Written by Kristen Michelle Nader <kristen.nader@helsinki.fi>
+# and Aleksandr Ianevski <aleksandr.ianevski@helsinki.fi> , April 2021
+#
+# Functions on this page:
+# sctype_source,run_scType,run_copyKat,run_SCEVAN,runEnsembl,visualize_ensembl_step
+#
+
